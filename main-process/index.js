176d73f902186940274ef14455bbdc07a02a7fe7
qiuchiyi <qiuchiyi@baidu.com>
2022-04-02 11:11:57 +0800

    init: project

--- main-process/index.js
+++ main-process/index.js
@@ -1,6 +1,8 @@
 // Modules to control application life and create native browser window
 const {
   app,
+  Tray,
+  Menu,
   ipcMain,
   BrowserWindow,
   globalShortcut,
@@ -11,6 +13,8 @@ const path = require('path');
 const { setWinOnActiveScreen } = require('./utils/screenHelper');
 const { ClipboardEx } = require('./utils/clipboardHelper');
 const { registerShortCut, unregisterShortCut } = require('./utils/globShortcutHelper');
+
+let tray = null;
 
 const APP_DATA_PATH = path.join(app.getAppPath('appData'), '.appData');
 if (!fs.existsSync(APP_DATA_PATH)) {
@@ -42,28 +46,61 @@ app.whenReady().then(() => {
   const win = createWin();
   const clipboardEx = new ClipboardEx({ dataPath: APP_DATA_PATH });
 
+  function showQlipboard() {
+    win.send('copyListUpdate', clipboardEx.orderCopyList);
+    // TODO 日志
+    // fs.writeFileSync(path.join(__dirname, 'log.js'), JSON.stringify(clipboardEx.orderCopyList));
+    setWinOnActiveScreen(win);
+    win.showInactive();
+    registerShortCut(win);
+    globalShortcut.register('Esc', () => {
+      hideWin(win);
+    });
+  }
+
   win.once('ready-to-show', () => {
     globalShortcut.register('Command+Shift+V', () => {
-      win.send('copyListUpdate', clipboardEx.orderCopyList);
-
-      // TODO 日志
-      // fs.writeFileSync(path.join(__dirname, 'log.js'), JSON.stringify(clipboardEx.orderCopyList));
-
-      setWinOnActiveScreen(win);
-      win.showInactive();
-
-      registerShortCut(win);
-
-      globalShortcut.register('Esc', () => {
-        hideWin(win);
-      });
+      showQlipboard();
     });
   });
 
-  // 测试用
-  globalShortcut.register('Command+Shift+B', () => {
-    clipboardEx.clearClipboard();
-  });
+  if (process.platform === 'darwin') {
+    tray = new Tray(path.join(__dirname, 'assets/iconTemplate.png'));
+  } else if (process.platform === 'linux') {
+    // tray = new Tray(path.join(__dirname, 'img', 'iconTemplate@2x.png'));
+  } else {
+    // tray = new Tray(path.join(__dirname, 'img', 'iconTemplate.png'));
+  }
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Open Qlipboard',
+      click() {
+        showQlipboard();
+      },
+    },
+    {
+      label: 'Hide Qlipboard',
+      click() {
+        hideWin(win);
+      },
+    },
+    {
+      label: 'Clear History',
+      click() {
+        clipboardEx.clearClipboard();
+      },
+    },
+    { type: 'separator' },
+    {
+      label: 'Quit Qlipboard',
+      click() {
+        app.quit();
+      },
+    },
+  ]);
+
+  tray.setToolTip('Olden');
+  tray.setContextMenu(contextMenu);
 
   ipcMain.on('selectedItem', (_event, index) => {
     clipboardEx.handlerItemByOrderIndex(index);
